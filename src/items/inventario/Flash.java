1c3ebb8e658ac77dd6ec91ee7cdbb7b058603109
Vicente <joserenatovi@gmail.com>
2015-05-26 15:32:43 -0300

    primeiro comitt da dupla jose + ninguem

---
+++ src/items/inventario/Flash.java
@@ -0,0 +1,12 @@
+package items.inventario;
+
+public class Flash extends GeneralItems {
+	
+	public Flash(String name, int number) {
+		super(name,number);
+	}
+	
+	public void effect() {
+//		chama funcao que ilumina o mapa todo por uma rodada
+	}
+}
