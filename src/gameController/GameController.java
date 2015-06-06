e93cb0e59b4ec770becb4e22ba0da816b7737566
Miguel Nicodemus <miguelnicodemus36@gmail.com>
2015-06-06 20:05:50 -0300

    Mais coisa sendo feito na Game Controller. Falta a main ainda.......................... Mas então, vejam se os castings acima são validos E galera, fluid web

--- src/gameController/GameController.java
+++ src/gameController/GameController.java
@@ -7,6 +7,18 @@ import org.newdawn.slick.Input;
 import player.IPlayerAction;
 import player.IPlayerMax;
 import player.PlayerDownAction;
+import player.PlayerUpAction;
+import player.PlayerLeftAction;
+import player.PlayerRightAction;
+import player.PlayerSetLighterAction;
+import player.PlayerShootDownAction;
+import player.PlayerShootUpAction;
+import player.PlayerShootLeftAction;
+import player.PlayerShootRightAction;
+import player.PlayerStickAction;
+import player.PlayerFlareAction;
+import player.PlayerUpAction;
+import player.PlayerWaitAction;
 import visual.ActionHandler;
 import visual.MapVisual;
 import visual.MapVisualActionFlare;
@@ -34,25 +46,90 @@ public class GameController implements IGameController {
 			playerShootRight, playerShootUp, playerStick, playerUp, playerWait;
 	private ArrayList<Entidade> entidades;
 	
-	private GameController() {
+	private GameController() { }
+	
+	private void bootGameController() {
 		entidades = new ArrayList<Entidade>();
 		
 		//TODO: Instanciar map e Player
+	
 		
 		//TODO: Instanciar as outras ações do player
-		playerDown = new PlayerDownAction();
+		playerDown = (IActionPlayerMapVisual) new PlayerDownAction();
 		playerDown.setKey(Input.KEY_DOWN);
 		playerDown.connect((IPlayerAction) player);
 		
+		playerLeft = (IActionPlayerMapVisual) new PlayerLeftAction();
+		playerLeft.setKey(Input.KEY_LEFT);
+		playerLeft.connect((IPlayerAction) player);
+		
+		playerRight = (IActionPlayerMapVisual) new PlayerRightAction();
+		playerRight.setKey(Input.KEY_RIGHT);
+		playerRight.connect((IPlayerAction) player);
+		
+		playerUp = (IActionPlayerMapVisual) new PlayerUpAction();
+		playerUp.setKey(Input.KEY_UP);
+		playerUp.connect((IPlayerAction) player);
+		
+		playerShootDown = (IActionPlayerMapVisual) new PlayerShootDownAction();
+		playerShootDown.setKey(Input.KEY_S);
+		playerShootDown.connect((IPlayerAction) player);
+		
+		playerShootLeft = (IActionPlayerMapVisual) new PlayerShootLeftAction();
+		playerShootLeft.setKey(Input.KEY_A);
+		playerShootLeft.connect((IPlayerAction) player);
+		
+		playerShootRight = (IActionPlayerMapVisual) new PlayerShootRightAction();
+		playerShootRight.setKey(Input.KEY_D);
+		playerShootRight.connect((IPlayerAction) player);
+		
+		playerShootUp = (IActionPlayerMapVisual) new PlayerShootUpAction();
+		playerShootUp.setKey(Input.KEY_W);
+		playerShootUp.connect((IPlayerAction) player);
+		
+		playerFlare = (IActionPlayerMapVisual) new PlayerFlareAction();
+		playerFlare.setKey(Input.KEY_R);
+		playerFlare.connect((IPlayerAction) player);
+		
+		playerStick = (IActionPlayerMapVisual) new PlayerStickAction();
+		playerStick.setKey(Input.KEY_E);
+		playerStick.connect((IPlayerAction) player);
+		
+		playerWait = (IActionPlayerMapVisual) new PlayerWaitAction();
+		playerWait.setKey(Input.KEY_G);
+		playerWait.connect((IPlayerAction) player);
+		
+		//TODO: Conectar as outras ações no handler depois de instanciar
 		
 		handler = new ActionHandler();
 		handler.connect(playerDown);
-		//TODO: Conectar as outras ações no handler depois de instanciar
+		handler.connect(playerUp);
+		handler.connect(playerLeft);
+		handler.connect(playerRight);
+		handler.connect(playerShootDown);
+		handler.connect(playerShootUp);
+		handler.connect(playerShootLeft);
+		handler.connect(playerShootRight);
+		handler.connect(playerFlare);
+		handler.connect(playerStick);
+		handler.connect(playerWait);
+		
+		//TODO: Conectar mapVisual as outras ações
 		
 		mapVisual = new MapVisual();
 		playerDown.connect(mapVisual);
-		//TODO: Conectar mapVisual as outras ações
-
+		playerUp.connect(mapVisual);
+		playerRight.connect(mapVisual);
+		playerLeft.connect(mapVisual);
+		playerShootDown.connect(mapVisual);
+		playerShootUp.connect(mapVisual);
+		playerShootLeft.connect(mapVisual);
+		playerShootRight.connect(mapVisual);
+		playerFlare.connect(mapVisual);
+		playerStick.connect(mapVisual);
+		playerWait.connect(mapVisual);
+		
+		//TODO: Conectar o mapVisual ao gameController
 		mapVisual.connect(this);
 	}
 
@@ -61,7 +138,9 @@ public class GameController implements IGameController {
 	}
 
 	public static void main(String[] args) {
-
+		int fase;
+		
+		
 	}
 
 	@Override
@@ -104,19 +183,19 @@ public class GameController implements IGameController {
 	@Override
 	public IPlayerMax getPlayer() {
 		// TODO Auto-generated method stub
-		return null;
+		return player;
 	}
 
 	@Override
 	public GameMap getMap() {
 		// TODO Auto-generated method stub
-		return null;
+		return map;
 	}
 
 	@Override
 	public ArrayList<Entidade> getEntidades() {
 		// TODO Auto-generated method stub
-		return null;
+		return entidades;
 	}
 
 	@Override
