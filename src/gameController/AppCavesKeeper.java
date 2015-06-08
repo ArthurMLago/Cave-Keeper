f4b55249ba3f82671c56f2e88b06291e549fb5fa
Miguel Nicodemus <miguelnicodemus36@gmail.com>
2015-06-08 19:52:24 -0300

    Criamos um applet para rodarmos a main

---
+++ src/gameController/AppCavesKeeper.java
@@ -0,0 +1,40 @@
+package gameController;
+import player.*;
+import visual.interfaces.*;
+import visual.*;
+import saveGame.*;
+import monster.*;
+import monster.Interfaces.*;
+import map.*;
+import items.*;
+import items.itemManagement.*;
+import items.interfaces.*;
+
+import anima.factory.IGlobalFactory;
+import anima.factory.context.componentContext.ComponentContextFactory;
+
+public class AppCavesKeeper {
+	public static void main(String argc[]) {
+		
+		try {
+			IGlobalFactory componentFactory = ComponentContextFactory.createGlobalFactory();
+			componentFactory.registerPrototype(Monster.class);
+			componentFactory.registerPrototype(Player.class);
+			componentFactory.registerPrototype(ItemManagement.class);
+			
+			IMonster compMonster = componentFactory.createInstance("<http://santanvarzea.com/monster.Monster>");
+			IPlayer compPlayer = componentFactory.createInstance("<http://santanvarzea.com/player.Player>");
+			IItemManagement compItemManagement = componentFactory.createInstance("<http://santanvarzea.com/items.itemManagement.ItemManagement>");
+		
+			GameController.getSharedInstance().conectar(compMonster, compPlayer, compItemManagement);
+			
+			while() {
+				
+			}
+		}
+		catch(Exception e) { 
+			e.printStackTrace();
+			System.out.println("Pau");
+		}
+	}
+}
--- src/gameController/GameController.java
+++ src/gameController/GameController.java
@@ -1,5 +1,7 @@
 package gameController;
 
+import items.interfaces.IItemManagement;
+
 import java.util.ArrayList;
 
 import org.newdawn.slick.Input;
@@ -11,90 +13,98 @@ import saveGame.*;
 import monster.*;
 import monster.Interfaces.*;
 import map.*;
+import anima.annotation.Component;
 import anima.component.IRequires;
 import anima.component.ISupports;
 import anima.component.InterfaceType;
+import anima.component.base.ComponentBase;
 
 /**
  * Componente que faz a conexao dos outros componentes
+ * 
  * @author MiguelHenrique
  *
  */
 
-
-public class GameController implements IGameController {
+public class GameController {
 	private static final GameController sharedInstance = new GameController();
-	private IPlayerMax player;
-	private GameMap map;
-	private IMapVisual mapVisual;
 	private Input command;
 	private ActionHandler handler;
-	private IActionPlayerMapVisual playerFlare,
-			playerSetLighter, playerShootDown, playerShootLeft,
-			playerShootRight, playerShootUp;
-	private IActionPlayer playerDown, playerLeft, playerRight, playerUp, playerStick, playerWait;
+	private IActionPlayerMapVisual playerFlare, playerSetLighter,
+			playerShootDown, playerShootLeft, playerShootRight, playerShootUp;
+	private IActionPlayer playerDown, playerLeft, playerRight, playerUp,
+			playerStick, playerWait;
+
 	private IMonster compMonster;
+	private IPlayer compPlayer;
+	private IItemManagement compItemManagement;
 	
-	private GameController() { }
+	private GameController() {
+	}
+
+	public void conectar(IMonster compMonster, IPlayer compPlayer, IItemManagement compItemManagement) {
+		this.compMonster = compMonster;
+		this.compPlayer = compPlayer;
+		this.compItemManagement = compItemManagement;
+	}
 	
 	private void bootGameController(int fase) {
 		Position playerSpawn;
-		
-		//TODO: Instanciar map, player e monstros
+
+		// TODO: Instanciar map, player e monstros
 		map = MapGenerator.sharedInstance().generateMap();
 		player = new Player();
 		compMonster = new Monster();
 		playerSpawn = map.getSpawnPoint(player, compMonster);
 		player.setSpawnPointPlayer(playerSpawn.getX(), playerSpawn.getY());
-		
-		
-		//TODO: Instanciar as outras ações do player
-		playerDown =  new PlayerDownAction();
+
+		// TODO: Instanciar as outras ações do player
+		playerDown = new PlayerDownAction();
 		playerDown.setKey(Input.KEY_DOWN);
 		playerDown.connect((IPlayerAction) player);
-		
-		playerLeft =  new PlayerLeftAction();
+
+		playerLeft = new PlayerLeftAction();
 		playerLeft.setKey(Input.KEY_LEFT);
 		playerLeft.connect((IPlayerAction) player);
-		
+
 		playerRight = new PlayerRightAction();
 		playerRight.setKey(Input.KEY_RIGHT);
 		playerRight.connect((IPlayerAction) player);
-		
+
 		playerUp = new PlayerUpAction();
 		playerUp.setKey(Input.KEY_UP);
 		playerUp.connect((IPlayerAction) player);
-		
+
 		playerShootDown = new PlayerShootDownAction();
 		playerShootDown.setKey(Input.KEY_S);
 		playerShootDown.connect((IPlayerAction) player);
-		
+
 		playerShootLeft = new PlayerShootLeftAction();
 		playerShootLeft.setKey(Input.KEY_A);
 		playerShootLeft.connect((IPlayerAction) player);
-		
+
 		playerShootRight = new PlayerShootRightAction();
 		playerShootRight.setKey(Input.KEY_D);
 		playerShootRight.connect((IPlayerAction) player);
-		
+
 		playerShootUp = new PlayerShootUpAction();
 		playerShootUp.setKey(Input.KEY_W);
 		playerShootUp.connect((IPlayerAction) player);
-		
+
 		playerFlare = new PlayerFlareAction();
 		playerFlare.setKey(Input.KEY_R);
 		playerFlare.connect((IPlayerAction) player);
-		
+
 		playerStick = new PlayerStickAction();
 		playerStick.setKey(Input.KEY_E);
 		playerStick.connect((IPlayerAction) player);
-		
+
 		playerWait = new PlayerWaitAction();
 		playerWait.setKey(Input.KEY_G);
 		playerWait.connect((IPlayerAction) player);
-		
-		//TODO: Conectar as outras ações no handler depois de instanciar
-		
+
+		// TODO: Conectar as outras ações no handler depois de instanciar
+
 		handler = new ActionHandler();
 		handler.connect(playerDown);
 		handler.connect(playerUp);
@@ -107,82 +117,22 @@ public class GameController implements IGameController {
 		handler.connect(playerFlare);
 		handler.connect(playerStick);
 		handler.connect(playerWait);
-		
-		//TODO: Conectar mapVisual as outras ações
-		
+
+		// TODO: Conectar mapVisual as outras ações
+
 		mapVisual = new MapVisual();
 		playerShootDown.connect(mapVisual);
 		playerShootUp.connect(mapVisual);
 		playerShootLeft.connect(mapVisual);
 		playerShootRight.connect(mapVisual);
 		playerFlare.connect(mapVisual);
-		
-		//TODO: Conectar o mapVisual ao gameController
+
+		// TODO: Conectar o mapVisual ao gameController
 		mapVisual.connect(this);
 	}
 
 	public static GameController getSharedInstance() {
 		return sharedInstance;
-	}
-	
-	public static void main(String argc[]) {
-		GameController gameController = GameController.getSharedInstance();
-		gameController.bootGameController(1);
-	}
-
-	@Override
-	public int addRef() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getInstanceId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends ISupports> T queryInterface(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends ISupports> T queryInterface(String arg0,
-			InterfaceType arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends ISupports> IRequires<T> queryReceptacle(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int release() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public IPlayerMax getPlayer() {
-		// TODO Auto-generated method stub
-		return player;
-	}
-
-	@Override
-	public GameMap getMap() {
-		// TODO Auto-generated method stub
-		return map;
-	}
-
-	@Override
-	public IMonster getEntidades() {
-		// TODO Auto-generated method stub
-		return compMonster;
 	}
 
 	@Override
