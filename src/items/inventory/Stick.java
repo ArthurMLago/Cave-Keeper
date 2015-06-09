31d7e9aa652c0b5cf1df64837e1b07ea5c48dd1a
Vicente <joserenatovi@gmail.com>
2015-06-09 02:23:26 -0300

    arrumando o item stick

--- src/items/inventory/Stick.java
+++ src/items/inventory/Stick.java
@@ -1,5 +1,8 @@
 package items.inventory;
 
+import player.IPlayerMax;
+import map.enumerations.TileType;
+import map.interfaces.IGameMap;
 import items.excecoes.OutofItemsException;
 
 /**
@@ -9,7 +12,8 @@ import items.excecoes.OutofItemsException;
 public class Stick extends GeneralItems {
 	
 	private static final long serialVersionUID = 1L;
-
+	private IGameMap map;
+	private IPlayerMax player;
 	public Stick(String name, int number) {
 		super(name,number);
 	}
@@ -17,9 +21,15 @@ public class Stick extends GeneralItems {
 	
 	public void effect() throws OutofItemsException {
 		super.effect();
+		int playerX = player.getX();
+		int playerY = player.getY();
+		if(map.getTileAt(playerX+1, playerY).getType() == TileType.Walkable
+	
+	/**
+     * chama funcao que checa o que envolta do carinha e detona a armadilha, caso exista
+     */
 		
-/**
- * chama funcao que checa o que envolta do carinha e detona a armadilha, caso exista
- */
+		
+		
 	}
 }
